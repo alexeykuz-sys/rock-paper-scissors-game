7bf70f91cd0666f6992db4c9d1bf7379fc91d468
alexeykuz-sys <alexeykuz@gmail.com>
2021-07-03 08:41:33 +0100

    adding grid

--- assets/script.js
+++ assets/script.js
@@ -1,137 +1,151 @@
-const gameBlock = document.querySelector('.game__block--wrapper');
+const gameBlock = document.querySelector('.game__block');
 const gameResult = document.querySelector('.game__result');
 const scoreNumber = document.querySelector('.game__header--score--number');
 const storedScore = localStorage.getItem("userScore");
 const rulesBtn = document.getElementById("game__rules--button");
 const closeBtn = document.querySelector(".modal__rules__title");
 const modal = document.querySelector('.game__rules--modal');
+const gamePick = document.querySelector('.game__block__pick')
 let userScore;
+const userPick = ['scissors', 'rock', 'paper']
+const gameChoice = document.querySelectorAll('.game__block--choice')
 
-window.onload = () => {
-    if (localStorage.userScore) {
-        userScore = storedScore;
-    } else {
-        userScore = 0;
+gameChoice.forEach(choice => choice.addEventListener('click', (event)=>{
+    let choiceName = event.target.dataset.name;
+    if(choiceName){
+        gameBlock.classList.remove('active');
+        gamePick.classList.add('active')
     }
-    updateScore();
-}
+    console.log(event.target.dataset.name);
+}))
 
-function updateScore() {
-    scoreNumber.innerHTML = `<p>${userScore}</p>`;
-}
 
-const saveToLocalStorage = () => {
-    localStorage.setItem("userScore", userScore);
-}
 
-gameBlock.addEventListener('click', event => {
-    if (event.target.className === 'game__block--choice') {
-        getWinner(event.target.id);
-    }
-});
+// window.onload = () => {
+//     if (localStorage.userScore) {
+//         userScore = storedScore;
+//     } else {
+//         userScore = 0;
+//     }
+//     updateScore();
+// }
 
-function compPick() {
-    possibleOptions = ['rock', 'paper', 'scissors'];
-    randomIndex = Math.floor(Math.random() * possibleOptions.length);
-    return possibleOptions[randomIndex];
-}
+// function updateScore() {
+//     scoreNumber.innerHTML = `${userScore}`;
+// }
 
-function getWinner(playerPick) {
-    removeAllChoices();
-    let computerPick = compPick();
-    displayCurrentChoices(playerPick, computerPick);
-    switch (playerPick) {
-        case 'paper':
-            if (computerPick === 'rock') {
-                win();
-            } else if (computerPick === 'scissors') {
-                lose();
-            } else {
-                draw();
-            }
-            break;
-        case 'rock':
-            if (computerPick === 'scissors') {
-                win();
-            } else if (computerPick === 'paper') {
-                lose();
-            } else {
-                draw();
-            }
-            break;
-        case 'scissors':
-            if (computerPick === 'paper') {
-                win();
-            } else if (computerPick === 'rock') {
-                lose();
-            } else {
-                draw();
-            }
-            break;
-        default:
-            alert("There's a problem");
-    }
-}
+// const saveToLocalStorage = () => {
+//     localStorage.setItem("userScore", userScore);
+// }
 
-function displayCurrentChoices(playerPick, computerPick) {
-    let playerChoice = document.getElementById(playerPick);
-    let computerChoice = document.getElementById(computerPick);
-    playerChoice.style.opacity = '1';
-    computerChoice.style.opacity = '1';
-}
+// gameBlock.addEventListener('click', event => {
+//     if (event.target.className === 'game__block--choice') {
+//         getWinner(event.target.id);
+//     }
+// });
 
-function removeAllChoices() {
-    buttonElements = document.getElementsByClassName("game__block--choice");
-    for (let i = 0; i < buttonElements.length; i++) {
-        buttonElements[i].style.opacity = '0';
-    }
-}
+// function compPick() {
+//     possibleOptions = ['rock', 'paper', 'scissors'];
+//     randomIndex = Math.floor(Math.random() * possibleOptions.length);
+//     return possibleOptions[randomIndex];
+// }
 
-function win() {
-    userScore++;
-    saveToLocalStorage();
-    updateScore();
-    gameResult.innerHTML = 
-    `<div>
-        <h1>YOU WON</h1>
-        <button id="play-again">Play Again</button>
-    </div>`;
-    const playAgainButton = document.querySelector('#play-again');
-    playAgainButton.addEventListener('click', () => {
-        location.reload();
-    });
-}
+// function getWinner(playerPick) {
+//     removeAllChoices();
+//     let computerPick = compPick();
+//     displayCurrentChoices(playerPick, computerPick);
+//     switch (playerPick) {
+//         case 'paper':
+//             if (computerPick === 'rock') {
+//                 win();
+//             } else if (computerPick === 'scissors') {
+//                 lose();
+//             } else {
+//                 draw();
+//             }
+//             break;
+//         case 'rock':
+//             if (computerPick === 'scissors') {
+//                 win();
+//             } else if (computerPick === 'paper') {
+//                 lose();
+//             } else {
+//                 draw();
+//             }
+//             break;
+//         case 'scissors':
+//             if (computerPick === 'paper') {
+//                 win();
+//             } else if (computerPick === 'rock') {
+//                 lose();
+//             } else {
+//                 draw();
+//             }
+//             break;
+//         default:
+//             alert("There's a problem");
+//     }
+// }
 
-function lose() {
-    if (userScore > 0) {
-        userScore--;
-    } else {
-        useScore = 0;
-    }
-    saveToLocalStorage();
-    updateScore();
-    gameResult.innerHTML = 
-    `<div>
-        <h1>YOU LOST</h1>
-        <button id="play-again">Play Again</button>
-    </div>`;
-    const playAgainButton = document.querySelector('#play-again');
-    playAgainButton.addEventListener('click', () => {
-        location.reload();
-    });
-}
+// function displayCurrentChoices(playerPick, computerPick) {
+//     let playerChoice = document.getElementById(playerPick);
+//     let computerChoice = document.getElementById(computerPick);
+//     playerChoice.style.opacity = '1';
+//     computerChoice.style.opacity = '1';
+// }
 
-function draw() {
-    gameResult.innerHTML = 
-    `<div>
-        <h1>IT'S A DRAW</h1>
-        <button id="play-again">Play Again</button>
-    </div>`;
-    const playAgainButton = document.querySelector('#play-again');
-    playAgainButton.addEventListener('click', () => {
-        location.reload();
-    });
-}
+// function removeAllChoices() {
+//     buttonElements = document.getElementsByClassName("game__block--choice");
+//     for (let i = 0; i < buttonElements.length; i++) {
+//         buttonElements[i].style.opacity = '0';
+//     }
+// }
+
+// function win() {
+//     userScore++;
+//     saveToLocalStorage();
+//     updateScore();
+//     gameResult.innerHTML = 
+//     `<div>
+//         <h1>YOU WON</h1>
+//         <button id="play-again">Play Again</button>
+//     </div>`;
+//     const playAgainButton = document.querySelector('#play-again');
+//     playAgainButton.addEventListener('click', () => {
+//         location.reload();
+//     });
+// }
+
+// function lose() {
+//     if (userScore > 0) {
+//         userScore--;
+//     } else {
+//         useScore = 0;
+//     }
+//     saveToLocalStorage();
+//     updateScore();
+//     gameResult.innerHTML = 
+//     `<div>
+//         <h1>YOU LOST</h1>
+//         <button id="play-again">Play Again</button>
+//     </div>`;
+//     const playAgainButton = document.querySelector('#play-again');
+//     playAgainButton.addEventListener('click', () => {
+//         location.reload();
+//     });
+// }
+
+// function draw() {
+//     gameResult.innerHTML = 
+//     `<div>
+//         <h1>IT'S A DRAW</h1>
+//         <button id="play-again">Play Again</button>
+//     </div>`;
+//     const playAgainButton = document.querySelector('#play-again');
+//     playAgainButton.addEventListener('click', () => {
+//         location.reload();
+//     });
+// }
 
 /*------Modal window-----*/
 
